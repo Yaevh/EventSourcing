a2b5952cb632455b440ede4382a75ef616c02d0a
T.M. <tomasz.piotr.malinowski@gmail.com>
2024-05-07 01:19:30 +0200

    moving some interfaces to Abstractions project

--- Yaevh.EventSourcing.Abstractions/IEventMetadata.cs
+++ Yaevh.EventSourcing.Abstractions/IEventMetadata.cs
@@ -27,21 +27,25 @@ namespace Yaevh.EventSourcing
         /// </summary>
         string EventName { get; }
         
-
         /// <summary>
         /// Uniquely identifies the aggregate the event belongs to.
         /// </summary>
         TAggregateId AggregateId { get; }
 
         /// <summary>
-        /// The name of the aggregate.
+        /// The name of the aggregate this event belongs to.
         /// </summary>
         string AggregateName { get; }
 
         /// <summary>
-        /// The index of the event with regard to the AggregateId.
+        /// The one-based index of the event with regard to the AggregateId.
         /// That is, the ordinal number of the event in a given aggregate.
         /// </summary>
+        /// <remarks>
+        /// Given an aggregate with multiple events,
+        /// the first event will have an <see cref="EventIndex"/> = 1,
+        /// the second will have an <see cref="EventIndex"/> = 2 and so on.
+        /// </remarks>
         long EventIndex { get; }
     }
 }
